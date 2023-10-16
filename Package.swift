8da788219b9c8906060f2f8eae67219600276224
workerprocess <workerprocess@gmail.com>
2023-10-16 17:28:37 +0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TodoKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TodoKit",
+            targets: ["TodoKit"]),
+    ],
+    dependencies: [
+        // Here we define our package's external dependencies
+        // and from where they can be fetched:
+        .package(
+            url: "https://github.com/johnsundell/files.git", 
+            from: "4.0.0"
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TodoKit",
+            dependencies: ["Files"]
+        ),
+        .testTarget(
+            name: "TodoKitTests",
+            dependencies: ["TodoKit"]),
+    ]
+)
